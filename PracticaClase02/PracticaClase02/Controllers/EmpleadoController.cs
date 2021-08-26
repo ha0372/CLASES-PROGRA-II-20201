785ff572c0236a656168b18add9adc1daf0c3785
ha0372 <69734226+ha0372@users.noreply.github.com>
2021-08-26 16:58:33 -0600

    saveEdit

--- PracticaClase02/PracticaClase02/Controllers/EmpleadoController.cs
+++ PracticaClase02/PracticaClase02/Controllers/EmpleadoController.cs
@@ -35,19 +35,91 @@ namespace PracticaClase02.Controllers
         }
 
 
+        //[HttpPost]
+        //public ActionResult Save(string nombre, string apellido, string dui, string direccion, string telefono, string correo, string cargo)
+        //{
+        //    using (EMPLEADOSEntities emple = new EMPLEADOSEntities())
+        //    {
+        //        inventario empleado = new inventario();
+
+        //        empleado.Empl_nombre = nombre;
+        //        empleado.Empl_apellido = apellido;
+        //        empleado.Empl_DUI = dui;
+        //        empleado.Empl_direccion = direccion;
+        //        empleado.Empl_tel = telefono;
+        //        empleado.Empl_email = correo;
+        //        empleado.Empl_cargo = cargo;
+        //        emple.inventario.Add(empleado);
+        //        emple.SaveChanges();
+        //    }
+
+        //    return Redirect("/Empleado/Empleado");
+        //}
         [HttpPost]
-        public ActionResult Save(string nombre)
+        public ActionResult Save(int id, string nombre, string apellido, string dui, string direccion, string telefono, string correo, string cargo)
         {
             using (EMPLEADOSEntities emple = new EMPLEADOSEntities())
             {
                 inventario empleado = new inventario();
 
-                empleado.Empl_nombre = nombre;
-                emple.inventario.Add(empleado);
-                emple.SaveChanges();
+                if (id == 0)
+                {
+                    empleado.Empl_nombre = nombre;
+                    empleado.Empl_apellido = apellido;
+                    empleado.Empl_DUI = dui;
+                    empleado.Empl_direccion = direccion;
+                    empleado.Empl_tel = telefono;
+                    empleado.Empl_email = correo;
+                    empleado.Empl_cargo = cargo;
+                    emple.inventario.Add(empleado);
+                    emple.SaveChanges();
+                }
+                else
+                {
+                    int update = id;
+                    inventario editar = emple.inventario.Where(x=> x.id_empleado == update).FirstOrDefault();
+                    editar.Empl_nombre = nombre;
+                    editar.Empl_apellido = apellido;
+                    editar.Empl_DUI = dui;
+                    editar.Empl_direccion = direccion;
+                    editar.Empl_tel = telefono;
+                    editar.Empl_email = correo;
+                    editar.Empl_cargo = cargo;
+                    //emple.inventario.Add(empleado);
+                    emple.SaveChanges();
+                }
+                
             }
 
             return Redirect("/Empleado/Empleado");
         }
+
+        [HttpPost]
+        public ActionResult EmpleadoSave(string nombre, string apellido, string dui, string direccion, string telefono, string correo, string cargo, int id= 0)
+        {
+            ViewBag.id = id;
+            ViewBag.nombre = nombre;
+            ViewBag.apellido = apellido;
+            ViewBag.dui = dui;
+            ViewBag.direccion = direccion;
+            ViewBag.telefono = telefono;
+            ViewBag.correo = correo;
+            ViewBag.cargo = cargo;
+
+
+            //= id;
+
+            return View("EmpleadoSave");
+        }
+
+        //public ActionResult Edit(int id)
+        //{
+
+
+        //    ViewBag.id = id;
+        //=id;
+        //    return View("EmpleadoSave");
+        //}
+
     }
 }
