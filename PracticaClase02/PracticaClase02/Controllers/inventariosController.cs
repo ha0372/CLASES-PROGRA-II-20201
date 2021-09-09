6b3a34a83cdd27a2a58fa66c5cbacbdbff8043a4
ha0372 <69734226+ha0372@users.noreply.github.com>
2021-09-09 09:51:31 -0600

    crud

---
+++ PracticaClase02/PracticaClase02/Controllers/inventariosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PracticaClase02.Models;
+
+namespace PracticaClase02.Controllers
+{
+    public class inventariosController : Controller
+    {
+        private EMPLEADOSEntities db = new EMPLEADOSEntities();
+
+        // GET: inventarios
+        public ActionResult Index()
+        {
+            return View(db.inventario.ToList());
+        }
+
+        // GET: inventarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            inventario inventario = db.inventario.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        // GET: inventarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: inventarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_empleado,Empl_nombre,Empl_apellido,Empl_DUI,Empl_direccion,Empl_tel,Empl_email,Empl_cargo")] inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.inventario.Add(inventario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(inventario);
+        }
+
+        // GET: inventarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            inventario inventario = db.inventario.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        // POST: inventarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_empleado,Empl_nombre,Empl_apellido,Empl_DUI,Empl_direccion,Empl_tel,Empl_email,Empl_cargo")] inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(inventario);
+        }
+
+        // GET: inventarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            inventario inventario = db.inventario.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        // POST: inventarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            inventario inventario = db.inventario.Find(id);
+            db.inventario.Remove(inventario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
